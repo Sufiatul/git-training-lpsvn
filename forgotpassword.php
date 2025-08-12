b52c4902c11a0aec8bdfa216baeb2aa37c16a498
noor sufiatul <sufiatulasni03@gmail.com>
2025-08-12 12:17:36 +0800

    update latest

---
+++ forgotpassword.php
@@ -0,0 +1,17 @@
+<?php
+// Configuration
+$db_host = 'localhost';
+$db_username = 'your_username';
+$db_password = 'your_password';
+$db_name = 'your_database';
+
+// Connect to database
+$conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Function to send password reset email
+function send_password_reset_email
